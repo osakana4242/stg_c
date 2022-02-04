9ee52377fb9d4f3fdbf8245570eb5684ce896091
hoge <example@example.com>
2022-02-04 23:19:37 +0900

    refactor: 不要なもの除去.

--- study_win32/study_win32.c
+++ study_win32/study_win32.c
@@ -1,7 +1,19 @@
 ﻿
-#include "math.h"
-#include "assert.h"
-#include "framework.h"
+// // SDKDDKVer.h をインクルードすると、利用できる最も高いレベルの Windows プラットフォームが定義されます。
+// 以前の Windows プラットフォーム用にアプリケーションをビルドする場合は、WinSDKVer.h をインクルードし、
+// サポートしたいプラットフォームに _WIN32_WINNT マクロを設定してから SDKDDKVer.h をインクルードします。
+#include <SDKDDKVer.h>
+
+#define WIN32_LEAN_AND_MEAN // Windows ヘッダーからほとんど使用されていない部分を除外する
+#include <windows.h>
+
+// C ランタイム ヘッダー ファイル
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <math.h>
+#include <assert.h>
 
 #define MAX_LOADSTRING 100
 
